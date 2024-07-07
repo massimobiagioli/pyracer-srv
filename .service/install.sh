18f8e76e069bc28f69b346f0806faec3e297432c
massimobiagioli <biagiolimassimo@gmail.com>
2024-07-07 17:05:50 +0200

    fix service

--- .service/install.sh
+++ .service/install.sh
@@ -3,15 +3,18 @@
 SERVICE_NAME="pyracer-srv.service"
 TEMPLATE_PATH="./$SERVICE_NAME"
 DESTINATION_PATH="/etc/systemd/system/$SERVICE_NAME"
+APP_PATH="/home/massimo/apps/pyracer-srv"
+
+echo "Copy all files to the app folder."
+mkdir -p $APP_PATH
+cp -a ../* $APP_PATH
 
 if systemctl is-active --quiet $SERVICE_NAME; then
 	echo "Stop service $SERVICE_NAME"
 	sudo systemctl stop $SERVICE_NAME
 fi
 
-if systemctl list-unit-files | grep -qw $SERVICE_NAME; then
-	echo "Service $SERVICE_NAME already installed"
-else
+if [ ! systemctl list-unit-files | grep -qw $SERVICE_NAME ]; then
 	if [ ! -f "$TEMPLATE_PATH" ]; then
 		echo "ERROR! $TEMPLATE_PATH does not exist"
 		exit 1
